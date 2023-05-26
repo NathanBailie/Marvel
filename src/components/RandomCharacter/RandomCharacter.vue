c229df752ed167fe3d9b3172a65b6cf25ef112a8
Nathan <nathanbailie.dev@gmail.com>
2023-05-26 15:49:52 +0300

    did the sedond part of the random charcker component

--- src/components/RandomCharacter/RandomCharacter.vue
+++ src/components/RandomCharacter/RandomCharacter.vue
@@ -6,25 +6,35 @@ export default {
 </script>
 
 <template>
-  <div class="rndChar">
-    <div class="rndChar__img"></div>
-    <div class="rndChar__info">
-      <h2>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
-        delectus nihil cupiditate? Aperiam eligendi minus dolores natus! Sed
-        iure sequi ex ab, deleniti odio perferendis earum nobis, incidunt
-        praesentium dignissimos.
-      </h2>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda rerum
-        optio laboriosam eum cumque exercitationem! Impedit dolorem, minus
-        provident quos ab nisi, dignissimos labore modi obcaecati voluptate,
-        velit ullam fuga!
-      </p>
-      <div class="rndChar__buttons">
-        <button class="rndChar__homePage">homepage</button>
-        <button class="rndChar__wiki">wiki</button>
+  <div class="wraper">
+    <div class="rndChar">
+      <div class="rndChar__img"></div>
+      <div class="rndChar__info">
+        <h2>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime
+          delectus nihil cupiditate? Aperiam eligendi minus dolores natus! Sed
+          iure sequi ex ab, deleniti odio perferendis earum nobis, incidunt
+          praesentium dignissimos.
+        </h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+          rerum optio laboriosam eum cumque exercitationem! Impedit dolorem,
+          minus provident quos ab nisi, dignissimos labore modi obcaecati
+          voluptate, velit ullam fuga!
+        </p>
+        <div class="rndChar__buttons">
+          <button class="rndChar__homePage">homepage</button>
+          <button class="rndChar__wiki">wiki</button>
+        </div>
       </div>
+    </div>
+
+    <div class="launcher">
+      <p>Random character for today!</p>
+      <p>Do you want to get to know him better?</p>
+      <p>Or choose another one</p>
+      <button>try it</button>
+      <img src="../../resources/img/bg/decor.png" alt="decor" />
     </div>
   </div>
 </template>
